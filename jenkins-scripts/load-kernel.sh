2d0f93a2d083505e7d9d28497b3d608bf4b49226
Andrew Vagin <avagin@openvz.org>
2014-05-22 08:14:46 -0400

    add load-kernel.sh
    This script compiles a new kernel and boots it.

---
+++ jenkins-scripts/load-kernel.sh
@@ -0,0 +1,16 @@
+set -e
+home_dir=$(dirname `readlink -f $0`)
+kernel_dir=$1
+cd $kernel_dir
+git fetch
+exit
+git checkout -f origin/master
+git describe HEAD
+git clean -dxf
+cp $home_dir/config .config
+make olddefconfig
+make -j 2
+make modules_install
+make install
+sync
+bash -x $home_dir/reboot.sh `make kernelrelease`
---
+++ jenkins-scripts/reboot.sh
@@ -0,0 +1,4 @@
+set -e
+kernel_release=$1
+kexec -l /boot/vmlinuz-$kernel_release --initrd=/boot/initramfs-$kernel_release.img --command-line="`cat /proc/cmdline`"
+kexec -e
