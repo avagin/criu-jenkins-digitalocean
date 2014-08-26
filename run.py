95a08cb096153f37c39ee6993e8a6f6df15cc3f6
Andrew Vagin <avagin@openvz.org>
2014-08-26 19:10:33 +0400

    be more verbose

--- run.py
+++ run.py
@@ -4,6 +4,12 @@ import digitalocean
 import digoc_config
 import requests
 
+def run_cmd(cmd):
+	print "Run - ", cmd
+	ret = os.system(cmd)
+	print "- exits with %d" % ret
+	return ret
+
 SSH = "ssh -oStrictHostKeyChecking=no -oBatchMode=yes -oServerAliveInterval=15 -oServerAliveCountMax=60 -oPreferredAuthentications=publickey"
 LOGS="/home/"
 #opts.image_name = "jenkins-3.11-template"
@@ -51,6 +57,7 @@ else:
 	raise Exception("Unable to find the %s sshkey", opts.size)
 
 def change_kernel(vm_id):
+	print "Boot the 3.11 kernel"
 	headers = {'Authorization':'Bearer ' + digoc_config.token}
 	headers['content-type'] = 'application/json'
 	#  {u'id': 453,
@@ -92,31 +99,33 @@ print droplet.ip_address
 
 ret = 0
 for i in xrange(60):
-	os.system("ip neig flush all");
-	if os.system("ping -c 1 -W 1 %s" % droplet.ip_address) == 0:
+	run_cmd("ip neig flush all");
+	if run_cmd("ping -c 1 -W 1 %s" % droplet.ip_address) == 0:
 		break;
 else:
 	ret = 1
 
 fname = "jenkins-%s.tar.gz" % datetime.datetime.now().strftime("%y-%m-%d-%H-%M")
 if ret == 0:
-	ret = os.system("scp -oStrictHostKeyChecking=no -oBatchMode=yes -r jenkins-scripts/ %s:" % droplet.ip_address)
+	ret = run_cmd("scp -oStrictHostKeyChecking=no -oBatchMode=yes -r jenkins-scripts/ %s:" % droplet.ip_address)
 
 if ret == 0 and opts.load_kernel:
-	ret = os.system("%s %s bash -x jenkins-scripts/load-kernel.sh /root/linux-next" % (SSH, droplet.ip_address))
+	ret = run_cmd("%s %s bash -x jenkins-scripts/load-kernel.sh /root/linux-next" % (SSH, droplet.ip_address))
 	if ret == 0:
-		os.system("%s %s kexec -e" % (SSH, droplet.ip_address))
+		run_cmd("%s %s kexec -e" % (SSH, droplet.ip_address))
 	stime = time.time()
 	while time.time() - stime < 60 and ret == 0:
-		if os.system("%s %s true" % (SSH, droplet.ip_address)) == 0:
+		if run_cmd("%s %s true" % (SSH, droplet.ip_address)) == 0:
 			break;
 		time.sleep(1)
+	else:
+		ret = 1
 
 if ret == 0:
-	ret = os.system("%s %s bash -x jenkins-scripts/jenkins-ct.sh jenkins.sh" % (SSH, droplet.ip_address))
+	ret = run_cmd("%s %s bash -x jenkins-scripts/jenkins-ct.sh jenkins.sh" % (SSH, droplet.ip_address))
 	if ret:
-		os.system("%s %s tar -czf %s -C criu ." % (SSH, droplet.ip_address, fname))
-		os.system("scp -oStrictHostKeyChecking=no -oBatchMode=yes %s:%s %s" % (droplet.ip_address, fname, LOGS))
+		run_cmd("%s %s tar -czf %s -C criu ." % (SSH, droplet.ip_address, fname))
+		run_cmd("scp -oStrictHostKeyChecking=no -oBatchMode=yes %s:%s %s" % (droplet.ip_address, fname, LOGS))
 droplet.shutdown()
 wait(droplet)
 #if ret:
