a7e2c322729394306f0731230a1f69d102124bf4
Andrey Vagin <avagin@openvz.org>
2015-01-15 01:25:43 +0300

    Detect lockdep warnings

--- jenkins-scripts/jenkins.sh
+++ jenkins-scripts/jenkins.sh
@@ -5,4 +5,5 @@ git clean -dxf &&
 make -j 2 &&
 bash -x test/zdtm.sh -C -x '.*\(maps01\|maps04\)' &&
 [ "`cat /proc/sys/kernel/tainted`" -eq 0 ] &&
+( dmesg -k | grep -A 30 ========================= && exit 1 || exit 0 ) &&
 true || exit 1
