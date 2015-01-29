8aca1171325afca17b452490f5dbef9cb7ec1e23
Andrey Vagin <avagin@openvz.org>
2015-01-29 16:35:22 +0300

    exclude *tcpbuf*

--- jenkins-scripts/jenkins.sh
+++ jenkins-scripts/jenkins.sh
@@ -3,7 +3,7 @@ git fetch &&
 git rebase origin/master &&
 git clean -dxf &&
 make -j 2 &&
-bash -x test/zdtm.sh -C -x '.*\(maps01\|maps04\)' &&
+bash -x test/zdtm.sh -C -x '.*\(maps01\|maps04\|tcpbuf\)' &&
 [ "`cat /proc/sys/kernel/tainted`" -eq 0 ] &&
 ( dmesg -k | grep -A 30 ========================= && exit 1 || exit 0 ) &&
 true || exit 1
