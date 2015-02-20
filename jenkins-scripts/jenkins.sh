94116f0e849707f0b0ebb660957f0d2feb53e45b
Andrey Vagin <avagin@openvz.org>
2015-02-20 09:41:55 +0300

    make test

--- jenkins-scripts/jenkins.sh
+++ jenkins-scripts/jenkins.sh
@@ -3,7 +3,9 @@ git fetch &&
 git rebase origin/master &&
 git clean -dxf &&
 make -j 2 &&
-bash -x test/zdtm.sh -C -x '.*\(maps01\|maps04\|tcpbuf\)' &&
+export ZDTM_ARGS="-x '.*\(maps01\|maps04\|tcpbuf\)'"
+make test &&
+#bash -x test/zdtm.sh -C -x '.*\(maps01\|maps04\|tcpbuf\)' &&
 [ "`cat /proc/sys/kernel/tainted`" -eq 0 ] &&
 ( dmesg -k | grep -A 30 ========================= && exit 1 || exit 0 ) &&
 true || exit 1
