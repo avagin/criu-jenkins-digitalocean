2a75950380c05a7e2b2de9f6b0a658368c12352b
Andrey Vagin <avagin@openvz.org>
2015-11-13 20:05:12 +0300

    Use zdtm.py

--- jenkins-scripts/jenkins.sh
+++ jenkins-scripts/jenkins.sh
@@ -3,10 +3,12 @@ git fetch &&
 git rebase origin/master &&
 git clean -dxf &&
 make -j 2 &&
-echo 0 > /sys/fs/cgroup/cpu/tasks
-export ZDTM_ARGS="-x '.*\(maps01\|maps04\|tcpbuf\)'"
-make test &&
+make -j 2 -C test/zdtm &&
+echo 0 > /sys/fs/cgroup/cpu/tasks &&
+./test/zdtm.py run --all --report report --parallel 2 -x 'maps04' &&
+true || exit 1
+
 #bash -x test/zdtm.sh -C -x '.*\(maps01\|maps04\|tcpbuf\)' &&
 [ "`cat /proc/sys/kernel/tainted`" -eq 0 ] &&
-( dmesg -k | grep -A 30 ========================= && exit 1 || exit 0 ) &&
-true || exit 1
+( dmesg -k | grep -A 30 ========================= && exit 1 || exit 0 )
+exit 0
