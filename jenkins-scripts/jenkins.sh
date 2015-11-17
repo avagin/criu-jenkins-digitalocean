a2d6df50c86e4634e9d7ff3f8bf4b351e0a82a07
Andrey Vagin <avagin@openvz.org>
2015-11-17 16:47:53 +0300

    Exculde static/maps01

--- jenkins-scripts/jenkins.sh
+++ jenkins-scripts/jenkins.sh
@@ -5,7 +5,7 @@ git clean -dxf &&
 make -j 2 &&
 make -j 2 -C test/zdtm &&
 echo 0 > /sys/fs/cgroup/cpu/tasks &&
-./test/zdtm.py run --all --report report --parallel 2 -x 'maps04' &&
+./test/zdtm.py run --all --report report --parallel 2 -x 'maps04' -x 'maps01' &&
 true || exit 1
 
 #bash -x test/zdtm.sh -C -x '.*\(maps01\|maps04\|tcpbuf\)' &&
